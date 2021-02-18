91c5ef7b9f184e6d0d26c717b4251936e0f0d7ae
elliana <ellianahendry@gmail.com>
2021-02-19 01:15:23 +0700

    Initial Commit

--- php/index.php
+++ php/index.php
@@ -8,14 +8,14 @@ exit;
 }
 echo "ini baru bisa bwang mwantap"."<br>" . PHP_EOL;
 echo "Info: " . mysqli_get_host_info($link) ."<br>" . PHP_EOL;
-int count = 0;
+int count = 0 ;
 $sql = "SELECT * FROM Trucorp.users";
 $result = $link->query($sql);
 if($result->num_rows > 0){
 while($row = $result->fetch_assoc()){
 count++;
 }
-echo "Hasil : ".count;
+echo "Hasil: ".count;
 }
 else {
 echo "gak ada data bwang";
